2aacf8a8b27d0015c9d05d4ad7a6b832d7806843
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 20:05:57 +0200

    Update p10.c

--- code4win/p10.c
+++ code4win/p10.c
@@ -1,1 +1,21 @@
-ECHO is on.
+#include <stdio.h>
+
+int main() {
+    int days;
+    
+    printf("Enter the days: ");
+    scanf("%d", &days);
+
+    // Calculattions
+    int years = (days / 365); 
+    int weeks = (days % 365) / 7;
+    int rem_days = days - ((years * 365) + (weeks * 7));
+
+
+    // results
+    printf("The Years are %d\n", years);
+    printf("The Weeks are %d\n", weeks);
+    printf("The Days are %d\n", rem_days);
+
+    return 0;
+}
