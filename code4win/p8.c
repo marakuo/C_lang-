5da86f8e360b2191523953a53edbb6d4ae5b4cd4
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 19:52:39 +0200

    Update p8.c

--- code4win/p8.c
+++ code4win/p8.c
@@ -1,1 +1,14 @@
-ECHO is on.
+#include <stdio.h>
+int main() {
+    
+    float temp;
+    
+     printf("Enter the Temp in celsius : ");
+     scanf("%f", &temp);
+     printf("The Temp in fahrenheit %f\n", (temp*1.8)+32);
+
+   
+    
+    
+    return 0;
+}
