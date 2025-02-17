e5f08eeb59f9cb571ecc023bd09693750f01f9c8
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 19:44:35 +0200

    Update p6.c

--- code4win/p6.c
+++ code4win/p6.c
@@ -1,1 +1,15 @@
-ECHO is on.
+#include <stdio.h>
+int main() {
+    
+    float radius;
+    
+     printf("Enter the Radius : ");
+     scanf("%f", &radius);
+     printf("The Diameter of circle is %f\n", 2*radius);
+     printf("The circumference  is %f\n", 2*3.14*radius);
+     printf("The Area of cirlce is %f", 3.14*radius*radius);
+   
+    
+    
+    return 0;
+}
