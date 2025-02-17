3cc68207cb873ef0844c488c326d580cc3cd56c2
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 19:48:09 +0200

    Update p7.c

--- code4win/p7.c
+++ code4win/p7.c
@@ -1,1 +1,15 @@
-ECHO is on.
+#include <stdio.h>
+int main() {
+    
+    float length;
+    
+     printf("Enter the length in cm : ");
+     scanf("%f", &length);
+     printf("The Length in meters is %f\n", length/100);
+     printf("The Length in kilometers is %f\n", length/100000);
+     
+   
+    
+    
+    return 0;
+}
