a09b869e9e02acfce077dd9857196a700581cb24
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 20:20:49 +0200

    Update p12.c

--- code4win/p12.c
+++ code4win/p12.c
@@ -1,1 +1,15 @@
-ECHO is on.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num;
+   
+    
+    printf("Enter the Number: ");
+    scanf("%d", &num);
+    int root = sqrt(num);
+    
+    printf("The Square root of %d is %d\n", num, root);
+
+    return 0;
+}
