40abc66bb5a9619feb0ea5eed185ecf3df395f6c
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 19:33:06 +0200

    Update p4.c

--- code4win/p4.c
+++ code4win/p4.c
@@ -1,1 +1,19 @@
- p5.c p6.c p7.c p8.c p10.c p11.c p12.c p16.c
+#include <stdio.h>
+int main() {
+    
+    int len,wid;
+    
+     printf("Enter the Length: ");
+     scanf("%d", &len);
+     printf("Enter the Width: ");
+     scanf(" %d", &wid);
+     
+     
+     printf("The perimeter of rectangle is %d", len*wid);
+     
+    
+   
+    
+    
+    return 0;
+}
