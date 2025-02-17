69c1fa57a237a4e22ca51fab13b525179ad5047a
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 19:17:55 +0200

    Update p3.c

--- code4win/p3.c
+++ code4win/p3.c
@@ -1,1 +1,21 @@
-ECHO is on.
+#include <stdio.h>
+int main() {
+    
+    int num1, num2;
+    
+     printf("Enter your 1st number: ");
+     scanf("%d", &num1);
+     printf("Enter your 2nd number: ");
+     scanf(" %d", &num2);
+     
+     printf("the sum is %d\n", num1+num2);
+     printf("the sub is %d\n", num1-num2);
+     printf("the multi is %d\n", num1*num2);
+     printf("the div is %d\n", num1/num2);
+     printf("the moulus is %d", num1%num2);
+    
+   
+    
+    
+    return 0;
+}
