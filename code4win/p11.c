860a100029a645523909dc958ab37b14ea447bc2
Malak Abdelhamed <marakuoo82@gmail.com>
2025-02-17 20:16:17 +0200

    Update p11.c

--- code4win/p11.c
+++ code4win/p11.c
@@ -1,1 +1,20 @@
-ECHO is on.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int base, power;
+   
+    
+    printf("Enter the base: ");
+    scanf("%d", &base);
+    printf("Enter the power: ");
+    scanf(" %d", &power);
+
+
+
+    long long ans = pow(base, power);
+    
+    printf("%d raised to the power of %d is %lld\n", base, power, ans);
+
+    return 0;
+}
